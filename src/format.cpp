8794bb65fe0ec8fe97e499757366486487766525
sai vemu <sais.vemu@gmail.com>
2022-09-02 11:30:31 -0700

    update format cpp file

--- src/format.cpp
+++ src/format.cpp
@@ -8,5 +8,6 @@ using std::to_string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 string Format::ElapsedTime(long seconds) {     
-    return to_string(seconds/3600) + ":" + to_string(seconds%3600/60) + ":" + to_string(seconds%60); 
+    string formatted_time = to_string(seconds/3600) + ":" + to_string(seconds%3600/60) + ":" + to_string(seconds%60);
+    return formatted_time; 
 }
