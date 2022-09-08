f6c4a3d88fc0f5deab2ae971afdecea5d991ae47
sai vemu <sais.vemu@gmail.com>
2022-09-07 22:01:41 -0700

    fix time format

--- src/format.cpp
+++ src/format.cpp
@@ -5,8 +5,21 @@
 using std::string;
 using std::to_string;
 
+#define hr_in_sec 3600
+#define min_in_sec 60
+
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
-string Format::ElapsedTime(long seconds) {     
-    return to_string(seconds/3600) + ":" + to_string(seconds%3600/60) + ":" + to_string(seconds%60);
+string Format::ElapsedTime(long seconds) { 
+    long hours = seconds/hr_in_sec;
+    seconds = (seconds%hr_in_sec);
+    long minutes = (seconds/min_in_sec);
+    seconds = seconds%min_in_sec;
+    string seconds_str = to_string(seconds);
+    string minutes_str = to_string(minutes);
+    string hours_str = to_string(hours);
+    seconds_str.insert(0, 2 - seconds_str.length(), '0');
+    minutes_str.insert(0, 2 - minutes_str.length(), '0');
+    hours_str.insert(0, 2 - hours_str.length(), '0');
+    return hours_str + ":" + minutes_str+ ":" + seconds_str;
 }
